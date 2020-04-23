36c51effab8cd49680366c2cc0b10a7d05b54fab
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2020-04-23 11:52:21 +0800

    mybook

--- mysite/mybook/urls.py
+++ mysite/mybook/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns = [
   path("", views.index, name="index"),
   path("search", views.search, name="search"),
-  path("more/<int:book_id>", views.more, name="more"),
+  path("book/<int:book_id>", views.more, name="more"),
   path("review/new/<int:book_id>", views.add_review, name="add_review"),
   path("register", views.register, name="register"),
   path("login", views.login, name="login"),
-  path("logout", views.logout, name="logout")
+  path("logout", views.logout, name="logout"),
+  path("book/<int:book_id>/review/<int:review_id>/edit", views.edit_review, name="edit_review")
 ]
--- mysite/mybook/views.py
+++ mysite/mybook/views.py
@@ -11,17 +11,17 @@ from .models import Book, Review
 def register(request):
   if request.method == 'GET':
     return render(request, "mybook/register.html")
-  else:
+  elif request.method == 'POST':
     try:
       username = request.POST["username"]
       password = request.POST["password"]
     except KeyError:
       return render(request, "mybook/error.html", {"message": "No selection."})
-    
     # check if username exists in database!!
     user = User.objects.create_user(username, 'example@thebeatles.com', password)
     user.save()
-    return render(request, "mybook/login.html")
+    return HttpResponseRedirect(reverse("login"))
+    #return render(request, "mybook/login.html", {"message": "Signed up."})
 
 def login(request):
   if request.method == 'GET':
@@ -49,7 +49,6 @@ def logout(request):
 
 
 #app routes
-
 def index(request):
   return render(request, "mybook/index.html")
 
@@ -88,14 +87,12 @@ def more(request, book_id):
         'work_ratings_count': 5,
         'average_rating': 4.0
     }
-    
     context = {
       "book": found_book,
       "api_book_info": api_book_info,
       "reviews": reviews,
       "username": request.user
     }
-    print(request.user)
     return render(request, "mybook/more.html", context)
 
 def add_review(request, book_id):
@@ -121,13 +118,46 @@ def add_review(request, book_id):
       content = request.POST["content"]
       rating = int(request.POST["rating"])
 
-
       review = Review(content=content, rating=rating, user=current_user, book=book)
       review.save()
       book.reviews.add(review)
-
       
       return HttpResponseRedirect(reverse("more", args=(book_id,)))
+
+
+def edit_review(request, review_id, book_id):
+  try:
+    found_review = Review.objects.get(pk=review_id)
+    found_book = Book.objects.get(pk=book_id)
+  except KeyError:
+    return render(request, "mybook/error.html", {"message": "Not Found."})
+  except Review.DoesNotExist:
+    raise Http404("Review does not exist.")
+  except Book.DoesNotExist:
+    raise Http404("Book does not exist.")
+  
+  #chosed_rating = found_review.rating
+  #only show other ratings
+  context = {
+    "review": found_review,
+    "book": found_book,
+  }
+
+  if request.method == "GET":
+    return render(request, "mybook/edit_review.html", context)
+  else:
+    content = request.POST["content"]
+    rating = request.POST["rating"]
+    found_review.content = content
+    found_review.rating = rating
+    found_review.save()
+
+    return HttpResponseRedirect(reverse("more", args=(book_id,)))
+    
+
+
+
+
       
 
 
